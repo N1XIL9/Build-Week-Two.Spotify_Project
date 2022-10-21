7fc8bae81a5e5d2eb34d421d54b09893f413257d
Aras Pourbijan <arasmidos@gmail.com>
2022-10-21 11:35:16 +0200

    cover box sistemato

--- assets/js/album.js
+++ assets/js/album.js
@@ -1,22 +1,18 @@
 window.onload = async () => {
-  let queryString = new URLSearchParams(window.location.search);
-  let id = queryString.get("id");
+    let queryString = new URLSearchParams(window.location.search);
+    let id = queryString.get("id");
 
-  let res = await fetch(
-    `https://striveschool-api.herokuapp.com/api/deezer/album/${id}`
-  );
-  let albumArray = await res.json();
-  console.log(albumArray);
+    let res = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`);
+    let albumArray = await res.json();
+    console.log(albumArray);
 
-  function secToMin(n) {
-    return new Date(n * 1000).toISOString().substr(14, 5);
-  }
+    function secToMin(n) {
+        return new Date(n * 1000).toISOString().substr(14, 5);
+    }
 
-  document.querySelector("#album-cover").innerHTML = `
+    document.querySelector("#album-cover").innerHTML = `
             
-            <img src="${
-              albumArray.cover
-            }" alt="" id="cover-img" class="shadow m-4 " />
+            <img src="${albumArray.cover}" alt="" id="cover-img" class="shadow m-4 " />
            
             <div id="album-info" class="ps-4 mt-5 text-dark">
                 <p class=" d-none d-sm-block">ALBUM</p>
@@ -24,15 +20,13 @@ window.onload = async () => {
                 <p>
                     <img src="https://img.freepik.com/premium-vector/hipster-frofile-hat-with-glasses_6229-762.jpg?w=2000" alt="" class="rounded img-fluid" width="20px" />
                     <span class="artist-name">
-                    <a href=./artist.html?id=${albumArray.artist.id}>${
-    albumArray.artist.name
-  }</a> - ${albumArray.release_date.substring(0, 4)} - ${
-    albumArray.nb_tracks
-  }, ${secToMin(albumArray.duration)} min</span>
+                    <a href=./artist.html?id=${albumArray.artist.id}>${albumArray.artist.name}</a> - ${albumArray.release_date.substring(0, 4)} - ${albumArray.nb_tracks}, ${secToMin(
+        albumArray.duration
+    )} min</span>
                 </p>
             </div>`;
 
-  document.querySelector("#navbar-left").innerHTML = `
+    document.querySelector("#navbar-left").innerHTML = `
     <img id="player-cover-b" src="${albumArray.cover_small}" alt="music" />
                     <div class="play-back">
                         <p id="title-song"></p>
@@ -40,37 +34,27 @@ window.onload = async () => {
                     </div>
                     <i class="bi bi-heart"></i>`;
 
-  for (let t of albumArray.tracks.data) {
-    let trackDiv = document.querySelector("#track-list-album");
+    for (let t of albumArray.tracks.data) {
+        let trackDiv = document.querySelector("#track-list-album");
 
-    trackDiv.innerHTML += `
+        trackDiv.innerHTML += `
         <div class="title-head text-light row mx-2 mt-4 track-div-box">
                             <div class="col row justify-content-start ">
-                                <div class="col-2">
-                                    <h6>${
-                                      albumArray.tracks.data.indexOf(t) + 1
-                                    }</h6>
+                                <div class="col-1">
+                                    <h6>${albumArray.tracks.data.indexOf(t) + 1}</h6>
                                 </div>
                     
                                 <div class="col links-of-list">
-                                    <div class="col song-to-play"><a class="song-name" onclick="myFunction('${
-                                      t.title
-                                    }','${t.preview}')">${t.title}</a></div>
-                                    <div class="col song-to-play text-white-50"><a href=./artist.html?id=${
-                                      t.artist.id
-                                    }>${t.artist.name}</a></div>
+                                    <div class="col song-to-play "><a class="song-name" onclick="myFunction('${t.title}','${t.preview}')">${t.title}</a></div>
+                                    <div class="col song-to-play text-white-50"><a href=./artist.html?id=${t.artist.id}>${t.artist.name}</a></div>
                                 </div>
                             </div>
-                            <div class="col-2 text-center text-white-50">${
-                              t.rank
-                            }</div>
-                            <div class="col-2 text-center text-white-50">${secToMin(
-                              t.duration
-                            )} min </div>
+                            <div class="col-2 text-center text-white-50">${t.rank}</div>
+                            <div class="col-2 text-center text-white-50">${secToMin(t.duration)} min </div>
                         </div>`;
-  }
+    }
 };
 
 function myFunction(nome, preview) {
-  document.getElementById("title-song").innerHTML = nome;
+    document.getElementById("title-song").innerHTML = nome;
 }
