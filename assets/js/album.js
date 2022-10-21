a30250a37acbdefdcb36d0f9db80633bb99a053e
Nicola Lerra <github.nicolalerra@gmail.com>
2022-10-21 18:07:17 +0200

    comm

--- assets/js/album.js
+++ assets/js/album.js
@@ -1,16 +1,16 @@
 window.onload = async () => {
-    let queryString = new URLSearchParams(window.location.search);
-    let id = queryString.get("id");
+  let queryString = new URLSearchParams(window.location.search);
+  let id = queryString.get("id");
 
-    let res = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`);
-    let albumArray = await res.json();
-    console.log(albumArray);
+  let res = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`);
+  let albumArray = await res.json();
+  console.log(albumArray);
 
-    function secToMin(n) {
-        return new Date(n * 1000).toISOString().substr(14, 5);
-    }
+  function secToMin(n) {
+    return new Date(n * 1000).toISOString().substr(14, 5);
+  }
 
-    document.querySelector("#album-cover").innerHTML = `
+  document.querySelector("#album-cover").innerHTML = `
             
             <img src="${albumArray.cover}" alt="" id="cover-img" class="shadow m-4 " />
            
@@ -20,13 +20,11 @@ window.onload = async () => {
                 <p>
                     <img src="https://img.freepik.com/premium-vector/hipster-frofile-hat-with-glasses_6229-762.jpg?w=2000" alt="" class="rounded img-fluid" width="20px" />
                     <span class="artist-name">
-                    <a href=./artist.html?id=${albumArray.artist.id}>${albumArray.artist.name}</a> - ${albumArray.release_date.substring(0, 4)} - ${albumArray.nb_tracks}, ${secToMin(
-        albumArray.duration
-    )} min</span>
+                    <a href=./artist.html?id=${albumArray.artist.id}>${albumArray.artist.name}</a> - ${albumArray.release_date.substring(0, 4)} - ${albumArray.nb_tracks}, ${secToMin(albumArray.duration)} min</span>
                 </p>
             </div>`;
 
-    document.querySelector("#navbar-left").innerHTML = `
+  document.querySelector("#navbar-left").innerHTML = `
     <img id="player-cover-b" src="${albumArray.cover_small}" alt="music" />
                     <div class="play-back">
                         <p id="title-song"></p>
@@ -34,10 +32,10 @@ window.onload = async () => {
                     </div>
                     <i id="heart-bg-player"class="bi bi-heart" onclick="changeClass()"></i>`;
 
-    for (let t of albumArray.tracks.data) {
-        let trackDiv = document.querySelector("#track-list-album");
+  for (let t of albumArray.tracks.data) {
+    let trackDiv = document.querySelector("#track-list-album");
 
-        trackDiv.innerHTML += `
+    trackDiv.innerHTML += `
         <div class="track-div-box text-light row mx-2 my-2">
                             <div class="col row justify-content-start ">
                                 <div class="col-1">
@@ -56,18 +54,18 @@ window.onload = async () => {
                             
                             <i class="col-2 bi bi-three-dots-vertical text-white-50 d-block d-sm-none"></i>
         </div>`;
-    }
+  }
 };
 
 function myFunction(nome, preview) {
-    document.getElementById("title-song").innerHTML = nome;
+  document.getElementById("title-song").innerHTML = nome;
 }
 
 let changeClass = function () {
-    document.getElementById("heart").classList.toggle("bi-heart");
-    document.getElementById("heart").classList.toggle("bi-heart-fill");
-    document.getElementById("heart-btm").classList.toggle("bi-heart");
-    document.getElementById("heart-btm").classList.toggle("bi-heart-fill");
-    document.getElementById("heart-bg-player").classList.toggle("bi-heart");
-    document.getElementById("heart-bg-player").classList.toggle("bi-heart-fill");
+  document.getElementById("heart").classList.toggle("bi-heart");
+  document.getElementById("heart").classList.toggle("bi-heart-fill");
+  document.getElementById("heart-btm").classList.toggle("bi-heart");
+  document.getElementById("heart-btm").classList.toggle("bi-heart-fill");
+  document.getElementById("heart-bg-player").classList.toggle("bi-heart");
+  document.getElementById("heart-bg-player").classList.toggle("bi-heart-fill");
 };
